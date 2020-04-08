67b3e9aaa9e9bf5fe209167e2a43cfe6ae6a778f
Adrian Tanase <tanase.adrian92@gmail.com>
2020-04-08 14:13:52 +0300

    config for the login page

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import { ValidationProvider } from 'vee-validate';
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import './assets/style/main.scss';
+
+Vue.component('ValidationProvider', ValidationProvider);
 
 Vue.config.productionTip = false
 
